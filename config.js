cde2397a52d1e03ef218442748e74624a030dd8c
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2022-05-02 14:19:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,14 +11,14 @@
 // thank you to ALL Bot creator
 // and thanks you to who support my Bot
 
-gc1 = 'https://instagram.com/mhdfakri_'
-gc2 = 'https://www.youtube.com/c/HokenBeusz'
+gc1 = 'https://instagram.com/_munavir._'
+gc2 = 'https://www.youtube.com/c/'
 gc3 = 'https://vt.tiktok.com/ZSeCJHbfA/'
 global.linkGC = ['https://chat.whatsapp.com/KTUF5TbtlWu4vOFnlDqCwg', ''] // ganti jadi group lu
-global.owner = ['62895328590064','62895327934887'] // Put your number here //owner eval
+global.owner = ['918590822912','918590822912'] // Put your number here //owner eval
 global.kontak = ['62895328590064','62895327934887'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['62895328590064','62895327934887'] // Premium user has unlimited limit
+global.prems = ['918590822912','918590822912'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
